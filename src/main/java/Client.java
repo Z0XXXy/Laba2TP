5d38edbe88695834e49b1de9e10429e24ee210b8
Z0XXXy <potyakin.a.ssau@gmail.com>
2021-10-20 14:09:14 +0400

    TP laba #2 Potyakin Artem Galinskaya Anastasiya

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,64 +3,66 @@ import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
 import java.io.*;
 import java.net.Socket;
+import javax.json.bind.JsonbException;
 
-public class Client implements Runnable{
-    private Socket socket;
-    private BufferedReader reader;
-    private BufferedReader in;
-    private BufferedWriter out;
-    private String message;
-    private Jsonb jsonb;
+public class Client implements Runnable {
 
-    public Client(int port) throws IOException {
-        socket=new Socket("localhost",port);
-        in =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out =new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        reader=new BufferedReader(new InputStreamReader(System.in));
-        jsonb= JsonbBuilder.create(new JsonbConfig());
-    }
-    public String getMessage(){
-        try{
-        String json=in.readLine();
-        String message=jsonb.fromJson(json,String.class);
-        return message;}
-        catch (IOException e){
-            System.err.println(e.getMessage());
-        }
-        return "error";
-    }
-    public void sendMessage(String message){
-        try {
-            out.write(jsonb.toJson(message));
-            out.newLine();
-            out.flush();
-        }
-        catch (IOException e){
-            System.err.println(e.getMessage());
-        }
-    }
-    @Override
-    public void run(){
-        try {
-            System.out.println(getMessage());
-            while (true) {
-                System.out.println(getMessage());
-                sendMessage(reader.readLine());
-                System.out.println(getMessage());
-            }
-        }
-        catch (IOException e){
-            System.err.println(e.getMessage());
-        }
-    }
+  private Socket socket;
+  private BufferedReader reader;
+  private BufferedReader in;
+  private BufferedWriter out;
+  private String message;
+  private Jsonb jsonb;
 
-    public static void main(String[] args) {
-        try {
-            Client player = new Client(4004);
-            player.run();
-        }
-        catch (IOException e){
-            System.out.println(e.getMessage());
-        }
+  public Client(int port) throws IOException {
+    socket = new Socket("localhost", port);
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    reader = new BufferedReader(new InputStreamReader(System.in));
+    jsonb = JsonbBuilder.create(new JsonbConfig());
+  }
+
+  public String getMessage() throws JsonbException{
+    try {
+      String json = in.readLine();
+      String message = jsonb.fromJson(json, String.class);
+      return message;
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
     }
+    return "error";
+  }
+
+  public void sendMessage(String message) throws JsonbException{
+    try {
+      out.write(jsonb.toJson(message));
+      out.newLine();
+      out.flush();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  @Override
+  public void run() {
+    try {
+      System.out.println(getMessage());
+      while (true) {
+        System.out.println(getMessage());
+        sendMessage(reader.readLine());
+        System.out.println(getMessage());
+      }
+    } catch (IOException| JsonbException|NumberFormatException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public static void main(String[] args) {
+    try {
+      Client player = new Client(4004);
+      player.run();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
